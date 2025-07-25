0be56dca065d45a705bc8fb7ee8c4c391ec40707
Slavi FE <slavidimitrov54@gmail.com>
2025-07-25 14:35:12 +0300

    feat(mongodb-mongoose) init databse connection and use mongoose for easier development

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app')
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-console.log(process.env.NODE_ENV);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => console.log('DB connection successful!'))
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const newTour = new Tour({
+  name: 'The Mushroom Adventure',
+  price: 497,
+})
+
+newTour.save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('Error saving tour: ', err)
+  })
+
 
 const PORT = process.env.PORT || 3000;
 
